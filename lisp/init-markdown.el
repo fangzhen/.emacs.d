696b4ed1e4fcde68839caf1842241d19791eeb40
fangzhen <fangzhenzyou@gmail.com>
2014-08-21 18:15:23 +0800

    disable whitespace-cleanup-mode for markdown mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,5 +3,5 @@
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
 (setq markdown-command "pandoc -f markdown -t html -s -c ~/.emacs.d/lisp/resources/style.css --mathjax --highlight-style kate")
-(add-hook 'markdown-mode (lambda () (whitespace-cleanup-mode nil)))
+(add-hook 'markdown-mode (lambda () (whitespace-cleanup-mode 0)))
 (provide 'init-markdown)
