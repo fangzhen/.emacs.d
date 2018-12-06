d5155be1a09952e381f23cc1c6c87ecd1db39230
Fang Zhen <zhen.fang@easystack.cn>
2018-12-06 18:18:56 +0800

    Update key-binding for tabbar ruler

--- lisp/init-tabbar-ruler.el
+++ lisp/init-tabbar-ruler.el
@@ -2,7 +2,8 @@
 (setq tabbar-ruler-global-tabbar t)
 (add-hook 'after-init-hook '(lambda () (tabbar-ruler-up)))
 
-(global-set-key [M-left] 'tabbar-backward-tab)
-(global-set-key [M-right] 'tabbar-forward-tab)
+(global-set-key (kbd "C-`") 'tabbar-forward-tab)
+(global-set-key (kbd "C-~") 'tabbar-backward-tab)
+(global-set-key (kbd "C-<escape>") 'tabbar-backward-tab)
 
 (provide 'init-tabbar-ruler)
