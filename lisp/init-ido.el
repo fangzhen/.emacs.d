48cb37215888f65a762ac212eefcc2e3aa1f1afb
fangzhen <fzhen.info@gmail.com>
2019-02-11 21:51:53 +0800

    Update to ido-completing-read+

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -8,16 +8,14 @@
 (setq ido-use-virtual-buffers t)
 (setq ido-auto-merge-work-directories-length -1)
 
-(when (eval-when-compile (>= emacs-major-version 24))
- (require-package 'ido-ubiquitous)
- (ido-ubiquitous-mode t))
+(require-package 'ido-completing-read+)
+(ido-ubiquitous-mode 1)
 
 ;; Use smex to handle M-x
-(when (eval-when-compile (>= emacs-major-version 24))
-  (require-package 'smex)
-  ;; Change path for ~/.smex-items
-  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-  (global-set-key [remap execute-extended-command] 'smex))
+(require-package 'smex)
+;; Change path for ~/.smex-items
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(global-set-key [remap execute-extended-command] 'smex)
 
 (require-package 'idomenu)
 
