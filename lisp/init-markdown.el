3899f10ba89b1d626725741997dc113e2d86ac53
fangzhen <fzhen.info@gmail.com>
2021-07-29 17:41:31 +0800

    Don't disable whitespace-cleanup-mode in markdown mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,8 +3,6 @@
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . gfm-mode) auto-mode-alist))
 (setq markdown-command "pandoc -f markdown -t html -s -c ~/.emacs.d/lisp/resources/style.css --mathjax --highlight-style kate")
-(add-hook 'markdown-mode (lambda () (whitespace-cleanup-mode 0)))
-(add-hook 'gfm-mode (lambda () (whitespace-cleanup-mode 0)))
 (setq markdown-gfm-use-electric-backquote nil)
 
 (provide 'init-markdown)
