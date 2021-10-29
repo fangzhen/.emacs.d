f9d888cbb091cea24e26e91332c250886250df1f
fangzhen <fzhen.info@gmail.com>
2021-10-29 20:05:53 +0800

    Enable save-place-mode

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,7 +1,8 @@
 (recentf-mode 1)
 (setq recentf-max-saved-items 1000
       recentf-exclude '("/tmp/" "/ssh:"))
-
 (add-hook 'find-file-hook 'recentf-save-list)
+
+(save-place-mode 1)
 
 (provide 'init-recentf)
