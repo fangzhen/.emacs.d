4f9d301a7deedf260c370ed313a7ce35c2b87fd7
fangzhen <fzhen.info@gmail.com>
2021-11-15 17:06:13 +0800

    Disable company-dabbrev-downcase
    Don't always lowercase candidates when use dabbrev backend.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,6 +2,9 @@
 (global-company-mode)
 (setq company-auto-complete-chars "qazwsxedcrfvtgbyhnujmikolp._-")
 (setq company-idle-delay .25)
+;; Don't always lowercase candidates when use dabbrev backend.
+(setq company-dabbrev-downcase nil)
+
 ;; override default color
 (defun company-face-tweak ()
   (let ((bg (face-attribute 'default :background)))
