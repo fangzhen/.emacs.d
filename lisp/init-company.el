39fb42d9fb83fa3d96535328a1ef787b784f56f5
fangzhen <fzhen.info@gmail.com>
2021-11-15 17:52:12 +0800

    Remove company-tooltip face tweaks.
    The new default face seems better now.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -5,22 +5,4 @@
 ;; Don't always lowercase candidates when use dabbrev backend.
 (setq company-dabbrev-downcase nil)
 
-;; override default color
-(defun company-face-tweak ()
-  (let ((bg (face-attribute 'default :background)))
-  (set-face-attribute 'company-tooltip nil
-                      :foreground (face-attribute 'default :foreground)
-                      :background (color-lighten-name bg 15))
-  (set-face-attribute 'company-scrollbar-bg nil :background (color-lighten-name bg 10))
-  (set-face-attribute 'company-scrollbar-fg nil :background (color-lighten-name bg 3))
-  (set-face-attribute 'company-tooltip-selection nil :inherit 'default :background (color-lighten-name bg 10))
-  (set-face-attribute 'company-tooltip-common nil :inherit 'default)))
-
-;; For the case that the init file runs after the frame has been created
-;; Call of emacs without --daemon option.
-(add-hook 'emacs-startup-hook 'company-face-tweak)
-;; For the case that the init file runs before the frame is created.
-;; Call of emacs with --daemon option.
-(add-hook 'server-after-make-frame-hook 'company-face-tweak)
-
 (provide 'init-company)
