0c186269e4b99d2a5d85df1a5df5f3b94e2bd9cd
fangzhen <fzhen.info@gmail.com>
2022-09-06 11:37:42 +0800

    Update theme to tsdh-light

--- lisp/init-face.el
+++ lisp/init-face.el
@@ -1,7 +1,4 @@
-(load-theme 'tango-dark)
+(load-theme 'tsdh-light)
 (setq font-use-system-font t)
-(custom-set-faces
- '(default ((t (:distant-foreground "black" :weight semi-bold :family "Source Code Pro" :height 113))))
- '(highlight ((t (:background "gray10" :foreground nil)))))
 
 (provide 'init-face)
