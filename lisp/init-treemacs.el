426f106b7472a29d807931d226a272c1d0337964
fangzhen <fzhen.info@gmail.com>
2024-06-25 19:44:10 +0800

    Remove treemacs-projectile

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -3,7 +3,4 @@
   (emacs-startup . (lambda() (if (not (daemonp)) (save-selected-window (treemacs-select-window)))))
   (server-after-make-frame . (lambda() (save-selected-window (treemacs-select-window))))
 )
-(use-package treemacs-projectile
-  :after treemacs projectile
-  )
 (provide 'init-treemacs)
