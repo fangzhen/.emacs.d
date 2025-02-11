2f246fdb7e34bb02c8516d69ba93a70e9f6fa46e
fangzhen <fzhen.info@gmail.com>
2025-02-11 11:59:00 +0800

    Remove lines introduced by mistake in init.el

--- init.el
+++ init.el
@@ -52,16 +52,3 @@
   (load custom-file-in-action))
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-vc-selected-packages
-   '((evedel :url "https://github.com/daedsidog/evedel" :branch "master"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
